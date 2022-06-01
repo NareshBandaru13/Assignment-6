6627ffc2f99a9940aa74301d4a1b893cbc6e9044
NareshBandaru13 <102959920+NareshBandaru13@users.noreply.github.com>
2022-06-01 16:09:45 +0530

    Add files via upload

---
+++ Assignment 6/code/plotting.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+  return 5-x
+xlist=np.linspace(-1,6,1000)
+ylist=f(xlist)
+plt.plot(xlist,ylist)
+
+plt.plot(5,0,marker="o",markeredgecolor="red",markerfacecolor="green")
+plt.plot(4,1,marker="o",markeredgecolor="red",markerfacecolor="green")
+plt.plot(3,2,marker="o",markeredgecolor="red",markerfacecolor="green")
+plt.plot(2,3,marker="o",markeredgecolor="red",markerfacecolor="green")
+plt.plot(1,4,marker="o",markeredgecolor="red",markerfacecolor="green")
+plt.plot(0,5,marker="o",markeredgecolor="red",markerfacecolor="green")
+
+plt.grid(b=True,axis="both")
+plt.show()
